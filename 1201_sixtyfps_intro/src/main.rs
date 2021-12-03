ae3a60ed77f969a3df0bba7e3ded90bddca6a4d3
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-03 20:30:32 +0900

    12.01 sixtyfps intro

---
+++ 1201_sixtyfps_intro/src/main.rs
@@ -0,0 +1,12 @@
+sixtyfps::sixtyfps!{
+    HelloWorld := Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
+fn main() {
+    HelloWorld::new().run();
+}
