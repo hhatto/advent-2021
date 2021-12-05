7a5b7e9716a4c35080e44b55466521aa30b4c4dc
Hideo Hattori <hhatto.jp@gmail.com>
2021-12-05 20:44:50 +0900

    1205 init

---
+++ 1205_ethersrs/src/main.rs
@@ -0,0 +1,16 @@
+use ethers::{prelude::*};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let provider = Provider::<Http>::try_from(
+        "http://localhost:8545/"
+    )?;
+
+    let address = provider.resolve_name("vitalik.eth").await?;
+    println!("{:?}", address);
+
+    let ens = provider.lookup_address(address).await?;
+    println!("{:?}", ens);
+
+    Ok(())
+}
